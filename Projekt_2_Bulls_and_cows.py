a99281dc21ed44f7570cd78b9bfe9445032c89ab
Michal Kavan <kavan1@centrum.cz>
2023-07-23 19:46:10 +0200

    Create Projekt_2_Bulls_and_cows.py

---
+++ Projekt_2_Bulls_and_cows.py
@@ -0,0 +1,50 @@
+#projekt_2.py: Druhý projekt do Engeto Online Python Akademie
+#author: Michal Kavan
+#email: kavan1@centrum.cz
+#discord: Michal K.#5207
+
+import random
+
+def print_welcome_message():
+    pozdrav = """Hi there!
+-----------------------------------------------
+I've generated a random 4-digit number for you.
+         Let's play a bulls and cows game.
+-----------------------------------------------"""
+    print(pozdrav)
+
+def generate_secret_number():
+    digits = random.sample(range(10), 4)
+    return ''.join(str(digit) for digit in digits)
+
+def get_user_guess():
+    guess = input("Enter a number:\n-----------------------------------------------\n>>> ")
+    while len(guess) != 4 or not guess.isdigit():
+        print("Invalid input. Please enter a 4-digit number.")
+        guess = input("Enter a number:\n-----------------------------------------------\n>>> ")
+    return guess
+
+def compare_numbers(secret_number, user_guess):
+    bulls = 0
+    cows = 0
+    for i in range(4):
+        if user_guess[i] == secret_number[i]:
+            bulls += 1
+        elif user_guess[i] in secret_number:
+            cows += 1
+    return bulls, cows
+
+def play_game():
+    secret_number = generate_secret_number()
+    print_welcome_message()
+    attempts = 0
+    while True:
+        user_guess = get_user_guess()
+        attempts += 1
+        bulls, cows = compare_numbers(secret_number, user_guess)
+        print(f"{bulls} bulls, {cows} cows")
+        if bulls == 4:
+            print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts!")
+            break
+
+play_game()
